2f78d563d75ff4c3d14975cb973b9c8e218b4dde
rollcorn <lazydaimon@gmail.com>
2022-09-21 00:04:26 +0300

    [Step#1] Init and show vehicles table

---
+++ src/test/java/com/project/autoparkweb/AutoparkWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.autoparkweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoparkWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
